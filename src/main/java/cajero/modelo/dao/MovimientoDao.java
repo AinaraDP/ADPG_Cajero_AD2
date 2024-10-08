1b2d15b155697a9f03f8bf353731cdca831b0a6e
Ainara De Pablo <122480987+AinaraDP@users.noreply.github.com>
2024-10-08 16:11:30 +0200

    subida

---
+++ src/main/java/cajero/modelo/dao/MovimientoDao.java
@@ -0,0 +1,16 @@
+package cajero.modelo.dao;
+
+import java.util.List;
+
+import cajero.modelo.entity.Cuenta;
+import cajero.modelo.entity.Movimiento;
+
+public interface MovimientoDao {
+
+	Movimiento findById(int idMovimiento);
+	List <Movimiento> findAll();
+	Movimiento insertarMovimiento(Movimiento movimiento);
+	List<Movimiento> findByCuenta(int idCuenta);
+	
+	
+}
